60c05800232fd497f6f23220cb8c3e2608c1d29f
Derrick Cui <cderrick126@gmail.com>
2023-11-13 00:21:47 -0800

    minor date errors

--- src/data/hacks.json.ts
+++ src/data/hacks.json.ts
@@ -15,13 +15,13 @@ const two: Template = {
   title: "CeloGive - User-friendly, compliant recurring donations",
   description:
     "EthMiami 2023 - Celo, Unlock Protocol, QuickNode, Tatum - $5,750",
-  date: "Nov 2022",
+  date: "Nov 2023",
 };
 const three: Template = {
   link: "https://github.com/trevor-trinh/blockchain-expense-managment",
   title: "SimpliSpend - Onchain defi expense management",
   description: "Franklin Templeton Blockchain Technology Contest 2023 - $7,000",
-  date: "Jun 2022",
+  date: "Jun 2023",
 };
 const four = {
   link: "https://ethglobal.com/showcase/relay-85njt",
@@ -34,13 +34,13 @@ const five = {
   link: "https://devfolio.co/projects/governnet-ed05",
   title: "GovernNet - Propogate governance decisions cross-chain",
   description: "LionHack ‘23 - Axelar - $1,000",
-  date: "15.06.2022",
+  date: "Apr 2023",
 };
 export const bytitle = {
-  one,
   two,
   three,
   four,
   five,
+  one,
 };
 export const hacks = Object.values(bytitle);
